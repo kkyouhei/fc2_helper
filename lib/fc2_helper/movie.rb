e97d9a5d8f8cbc204cbeb4a2d1071d33ef03b2df
kkyouhei <hope637@gmail.com>
2015-01-01 23:05:17 +0900

    動画埋め込みタグ用のメンバ変数を追加

--- lib/fc2_helper/movie.rb
+++ lib/fc2_helper/movie.rb
@@ -2,7 +2,19 @@
 
 module Fc2Helper
   class Movie
-    attr_accessor :title, :upid, :seconds
+    attr_accessor :title, :upid, :seconds, :external_link_tag
+    SRC = 'http://static.fc2.com/video/js/outerplayer.min.js'
+    TK = 'TVRJM056WTNOamc9'
+    SJ = '45000'
+    D = '1494'
+    W = '448'
+    H = '310'
+    CHARSET = 'UTF-8'
+
+    def external_link_tag
+      external_link_tag = "<script src='#{SRC}' url='http://video.fc2.com/ja/a/content/%s/' tk='#{TK}' tl='%s' sj='#{SJ}' d='#{D}' w='#{W}' h='#{H}'  suggest='on' charset='#{CHARSET}'></script>"
+      sprintf(external_link_tag, @upid, @title)
+    end
   end
 
 end
