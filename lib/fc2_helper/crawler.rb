dd89503d9004e1b093913045740384ca20f0094c
kkyouhei <hope637@gmail.com>
2015-01-01 21:32:58 +0900

    定数が呼び出せないバグを修正

--- lib/fc2_helper/crawler.rb
+++ lib/fc2_helper/crawler.rb
@@ -12,7 +12,7 @@ module Fc2Helper
     attr_reader :page_max
     attr_reader :movies
 
-    after_initialize do
+    def initialize
       @page_index = 1
       @page_max = 1
       @movies = {}
@@ -29,39 +29,39 @@ module Fc2Helper
     end
 
     def get_adult_ranking_weekly(index=1)
-      @crawler_url = FC2_ADULT_RANKING_WEEKLY
+      @crawler_url = Env::FC2_ADULT_RANKING_WEEKLY
       @page_index = index
-      movie_data = get_html(sprintf(FC2_ADULT_RANKING_WEEKLY, index))
+      movie_data = get_html(sprintf(Env::FC2_ADULT_RANKING_WEEKLY, index))
       parsed_movie_data(movie_data)
     end
     def get_adult_ranking_monthly(index=1)
-      @crawler_url = FC2_ADULT_RANKING_MONTHLY
+      @crawler_url = Env::FC2_ADULT_RANKING_MONTHLY
       @page_index = index
-      movie_data = get_html(sprintf(FC2_ADULT_RANKING_MONTHLY, index))
+      movie_data = get_html(sprintf(Env::FC2_ADULT_RANKING_MONTHLY, index))
       parsed_movie_data(movie_data)
     end
     def get_adult_ranking_yearly(index=1)
-      @crawler_url = FC2_ADULT_RANKING_YEARLY
+      @crawler_url = Env::FC2_ADULT_RANKING_YEARLY
       @page_index = index
-      movie_data = get_html(sprintf(FC2_ADULT_RANKING_YEARLY, index))
+      movie_data = get_html(sprintf(Env::FC2_ADULT_RANKING_YEARLY, index))
       parsed_movie_data(movie_data)
     end
     def get_adult_popular(index=1)
-      @crawler_url = FC2_ADULT_POPULAR
+      @crawler_url = Env::FC2_ADULT_POPULAR
       @page_index = index
-      movie_data = get_html(sprintf(FC2_ADULT_POPULAR, index))
+      movie_data = get_html(sprintf(Env::FC2_ADULT_POPULAR, index))
       parsed_movie_data(movie_data)
     end
     def get_adult_new(index=1)
-      @crawler_url = FC2_ADULT_NEW
+      @crawler_url = Env::FC2_ADULT_NEW
       @page_index = index
-      movie_data = get_html(sprintf(FC2_ADULT_NEW, index))
+      movie_data = get_html(sprintf(Env::FC2_ADULT_NEW, index))
       parsed_movie_data(movie_data)
     end
     def get_adult_search(keyword, index=1)
-      @crawler_url = sprintf(FC2_ADULT_SEARCH, keyword)
+      @crawler_url = sprintf(Env::FC2_ADULT_SEARCH, keyword)
       @page_index = index
-      movie_data = get_html(sprintf(FC2_ADULT_SEARCH, keyword, index))
+      movie_data = get_html(sprintf(Env::FC2_ADULT_SEARCH, keyword, index))
       parsed_movie_data(movie_data)
     end
 
@@ -69,28 +69,27 @@ module Fc2Helper
     def get_html(url)
       begin
         return Nokogiri.HTML(open(url).read)
-      rescue Excaption => e
+      rescue Exception => e
         raise e
       end
     end
 
     def parsed_movie_data(html)
       movies = Movies.new
-      html.xpath('//div[@class="video_list_renew_thumb"]').each do |post|
+      html.xpath('//div[@id="main"]').each do |post|
         begin
           post.search('div[@class="video_thumb_small clsThumbToAlbum"]').each do |div|
             movie = Movie.new
-
             movie.upid = div.attribute("upid").value
-            movie.title = div.children.children.attribute('title').value
+            movie.title = div.children.attribute('title').value
 
-            time = div.children.attribute('data-duration').value.sprit(":")
+            time = div.attribute('data-duration').value.split(":")
             seconds = time[1].to_i + time[0].to_i * 60
             movie.seconds = seconds
 
             movies.append_movie(movie)
           end
-        rescue Excaption => e
+        rescue Exception => e
           raise e
         end
       end
