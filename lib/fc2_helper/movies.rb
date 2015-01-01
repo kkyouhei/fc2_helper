0e4933e5d3e1568e5afebef46cd76a927f29ebd9
kkyouhei <hope637@gmail.com>
2015-01-01 21:33:33 +0900

    Arrayの使い方がいろいろ間違っていた

--- lib/fc2_helper/movies.rb
+++ lib/fc2_helper/movies.rb
@@ -3,24 +3,25 @@
 module Fc2Helper
   class Movies
     attr_reader :index
-    @crawler
+    attr_reader :movie_list
 
-    after_initialize do
-      @movies = {}
+    def initialize
+      @movie_list = Array.new
       @index = 0
     end
 
     def append_movie(movie)
-      @movies << movie
+      @movie_list.push(movie)
     end
 
     def next?
-      @movie.length > @index
+      @movie_list.length > @index
     end
 
     def next
+      return nil unless next?
       @index += 1
-      @movies[@index]
+      @movie_list[@index]
     end
 
   end
